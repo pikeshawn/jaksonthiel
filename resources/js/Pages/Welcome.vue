4bc0625d5fd71060904bd9ccca13525130aab114
Shawn Pike <pike.shawn@gmail.com>
2021-06-15 11:29:47 -0600

    added dr time

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -61,9 +61,9 @@
                 </PopoverPanel>
             </transition>
         </Popover>
-        <main class="lg:relative">
+        <main class="lg:relative flex flex-col">
             <div class="mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-48 lg:text-left">
-                <div class="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
+                <div class="px-4 sm:px-8 xl:pr-16">
                     <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
                         <span class="block xl:inline">Welcome to my life as I know it</span>
                         {{ ' ' }}
@@ -88,12 +88,25 @@
 <!--                    </div>-->
                 </div>
             </div>
-            <div class="relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full">
-                <video class="absolute inset-0 w-full h-full object-cover" width="320" height="240" controls>
-                    <source src="/img/Project_06-061_HD_720p_mp4.mov">
-                    Your browser does not support the video tag.
-                </video>
+
+            <div class="flex">
+                <div class="relative w-full h-64 sm:h-72 md:h-96 lg:inset-y-0 lg:right-0 lg:w-1/2">
+                    <video class="absolute inset-0 w-full h-full object-cover" width="320" height="240" controls>
+                        <source src="/img/Project_06-061_HD_720p_mp4.mov">
+                        Your browser does not support the video tag.
+                    </video>
+                </div>
+
+
+                <div class="relative w-full h-64 sm:h-72 md:h-96 lg:inset-y-0 lg:right-0 lg:w-1/2">
+                    <video class="absolute inset-0 w-full h-full object-cover" width="320" height="240" controls>
+                        <source src="/img/DrTime.mov">
+                        Your browser does not support the video tag.
+                    </video>
+                </div>
             </div>
+
+
         </main>
     </div>
 </template>
